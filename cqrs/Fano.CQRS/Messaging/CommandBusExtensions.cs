3ff1d49519da560e13127724217a30d071f4d6c5
Eyassu Getachew <eyassug@gmail.com>
2017-12-01 11:05:24 +0300

    added interfaces and extensiong for messaging infrastructure

---
+++ cqrs/Fano.CQRS/Messaging/CommandBusExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Fano.CQRS.Messaging
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Provides usability overloads for <see cref="ICommandBus"/>
+    /// </summary>
+    public static class CommandBusExtensions
+    {
+        public static async Task SendAsync(this ICommandBus bus, ICommand command)
+        {
+            await bus.SendAsync(new Envelope<ICommand>(command));
+        }
+
+        public static async Task SendAsync(this ICommandBus bus, IEnumerable<ICommand> commands)
+        {
+            await bus.SendAsync(commands.Select(x => new Envelope<ICommand>(x)));
+        }
+    }
+}
