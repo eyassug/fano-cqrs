25273ddd359c2521545c1c2d54930c514825f8b1
Eyassu Getachew <eyassug@gmail.com>
2018-11-08 23:13:52 +0300

    removed non-generic IEventSourced interface, changed IEventSourcedRepository to use generic Id types, added IAsyncEventSourcedRepository

---
+++ source/Fano.CQRS.EventSourcing/IAsyncEventSourcedRepository.cs
@@ -0,0 +1,30 @@
+namespace Fano.CQRS.EventSourcing
+{
+    using System;
+    using System.Threading.Tasks;
+    
+    public interface IAsyncEventSourcedRepository<T, TKey> where T : IEventSourced<TKey>
+    {
+        // <summary>
+        /// Tries to retrieve the event sourced entity.
+        /// </summary>
+        /// <param name="id">The id of the entity</param>
+        /// <returns>The hydrated entity, or null if it does not exist.</returns>
+        Task<T> FindAsync(TKey id);
+
+        /// <summary>
+        /// Retrieves the event sourced entity asynchronously.
+        /// </summary>
+        /// <param name="id">The id of the entity</param>
+        /// <returns>The hydrated entity</returns>
+        /// <exception cref="EntityNotFoundException">If the entity is not found.</exception>
+        Task<T> GetAsync(TKey id);        
+
+        /// <summary>
+        /// Saves the event sourced entity asynchronously.
+        /// </summary>
+        /// <param name="eventSourced">The entity.</param>
+        /// <param name="correlationId">A correlation id to use when publishing events.</param>
+        Task SaveAsync(T eventSourced, string correlationId);
+    }
+}
--- source/Fano.CQRS.EventSourcing/IEventSourcedRepository.cs
+++ source/Fano.CQRS.EventSourcing/IEventSourcedRepository.cs
@@ -3,20 +3,20 @@
     using System;
     using System.Threading.Tasks;
     
-    public interface IEventSourcedRepository<T> where T : IEventSourced
+    public interface IEventSourcedRepository<T, TKey> where T : IEventSourced<TKey>
     {
         /// <summary>
         /// Tries to retrieve the event sourced entity.
         /// </summary>
         /// <param name="id">The id of the entity</param>
         /// <returns>The hydrated entity, or null if it does not exist.</returns>
-        T Find(Guid id);
+        T Find(TKey id);
         // <summary>
         /// Tries to retrieve the event sourced entity.
         /// </summary>
         /// <param name="id">The id of the entity</param>
         /// <returns>The hydrated entity, or null if it does not exist.</returns>
-        Task<T> FindAsync(Guid id);
+        Task<T> FindAsync(TKey id);
         /// <summary>
         /// Retrieves the event sourced entity asynchronously.
         /// </summary>
@@ -31,7 +31,7 @@
         /// <param name="id">The id of the entity</param>
         /// <returns>The hydrated entity</returns>
         /// <exception cref="EntityNotFoundException">If the entity is not found.</exception>
-        Task<T> GetAsync(Guid id);
+        Task<T> GetAsync(TKey id);
 
         /// <summary>
         /// Saves the event sourced entity.
